d5d4601d229d77c42eb07e93f9dc909835027187
Yan Gorshkov <gorshkov.yan1998@gmail.com>
2018-07-03 18:24:37 +0300

    Was created folder for class-models and class "Comment". (fixes #1)

---
+++ bsa2018-DataStructures/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bsa2018_DataStructures.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public DateTime CreateAt { get; set; }
+        public string Body { get; set; }
+        public int UserId { get; set; }
+        public int PostId { get; set; }
+        public int Likes { get; set; }
+    }
+}
