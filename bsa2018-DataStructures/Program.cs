9cbfbcb057d994be4490552398ac22a82a21f84f
Yan Gorshkov <gorshkov.yan1998@gmail.com>
2018-07-04 02:22:11 +0300

    Was implemented first query (fixes #10)

--- bsa2018-DataStructures/Program.cs
+++ bsa2018-DataStructures/Program.cs
@@ -1,20 +1,28 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 using bsa2018_DataStructures.Models;
 
 namespace bsa2018_DataStructures
 {
     class Program
     {
+        private static List<User> users;
         public static void Main(string[] args)
         {
             LoadData load = new LoadData();
-            List<User> users = load.LoadAsync().Result;
-            Console.WriteLine("Downloading page...");
+            users = load.LoadAsync().Result;
+            FirstQuery(1);
+
             Console.ReadLine();
+        }
+
+        public static void FirstQuery(int idUser)
+        {
+            int count = (from user in users
+                         where user.Id == idUser
+                         select user.Posts).Count();
+            Console.WriteLine($"Count of posts = {count}");
         }
     }
 }
